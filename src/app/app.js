30021fe769c516212cbce55f6a96acbb962bd028
Nalin Ilango <nalin@theteamie.com>
2016-12-07 00:46:02 +0800

    Adding resizable with custom handlers

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,7 @@
 import 'jquery-ui-css/core.css';
 import 'jquery-ui-css/menu.css';
 import 'jquery-ui-css/draggable.css';
+import 'jquery-ui-css/resizable.css';
 import 'jquery-ui-css/theme.css';
 
 import angular from 'angular';
--- src/app/directives/fl-item.js
+++ src/app/directives/fl-item.js
@@ -1,6 +1,9 @@
 import $ from 'jquery';
 import 'jquery-ui/draggable';
+import 'jquery-ui/resizable';
 import Item from '../classes/Item'
+
+import '../../style/resizable.css';
 
 export default function (mapService) {
   return {
@@ -20,33 +23,55 @@ export default function (mapService) {
     link: function (scope, element, attrs, [flContainer, flItem]) {
       element.addClass('fl-item');
 
-      const indicator = $('<div>').addClass('fl-drag-indicator').addClass('fl-item');
-      const clone = $('<div>').addClass('fl-drag-clone');
-      clone.append(indicator);
-      element.draggable({
-        cursor: 'move',
-        cancel: '[fl-item] > *',
-        containment: '[fl-container]',
-        start: () => {
-          element.children().clone().appendTo(indicator);
-        },
-        drag: (event, ui) => {
-          const indicatorPos = mapService.pos2px(mapService.px2pos(ui.position));
-          indicator.css({
-            left: indicatorPos.left - ui.position.left,
-            top: indicatorPos.top - ui.position.top
-          });
-        },
-        stop: (event, ui) => {
-          indicator.empty();
-          flContainer.moveItem(flItem, ui);
-        },
-        helper: function () {
-          clone.css(mapService.layout2px(flItem.item));
-          indicator.css(mapService.layout2px(flItem.item));
-          return clone;
-        }
-      });
+      (() => {
+        const indicator = $('<div>').addClass('fl-drag-indicator fl-item');
+        const clone = $('<div>').addClass('fl-drag-clone');
+        clone.append(indicator);
+        element.draggable({
+          cursor: 'move',
+          cancel: '[fl-item] > *',
+          containment: 'parent',
+          start: () => {
+            element.children().clone().appendTo(indicator);
+          },
+          drag: (event, ui) => {
+            const indicatorPos = mapService.pos2px(mapService.px2pos(ui.position));
+            indicator.css({
+              left: indicatorPos.left - ui.position.left,
+              top: indicatorPos.top - ui.position.top
+            });
+          },
+          stop: (event, ui) => {
+            indicator.empty();
+            flContainer.moveItem(flItem, ui);
+          },
+          helper: function () {
+            clone.css(mapService.layout2px(flItem.item));
+            indicator.css(mapService.layout2px(flItem.item));
+            return clone;
+          }
+        });
+      })();
+
+      (() => {
+        const handles = {};
+        ['e', 'se', 's', 'sw', 'w'].forEach(direction => {
+          handles[direction] = $('<div>')
+            .addClass(`ui-resizable-handle ui-resizable-${direction} fl-resizable`)
+            .appendTo(element);
+        });
+
+        element.resizable({
+          containment: 'parent',
+          handles,
+          start: (...args) => {
+            console.debug('start', args);
+          },
+          stop: (...args) => {
+            console.debug('stop', args);
+          }
+        });
+      })();
 
       flContainer.initItem(flItem);
     }
