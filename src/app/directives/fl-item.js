0f06f3426ac64c86306c8ebdcfb1f12a53503793
Nalin Ilango <nalin@theteamie.com>
2017-01-28 21:31:18 +0800

    Reverting previous change; removing styles which were making assumptions of the content

--- src/app/directives/fl-item.js
+++ src/app/directives/fl-item.js
@@ -148,7 +148,7 @@ export default function () {
         if (flItem.getHeight) {
           const pixels = flContainer.mapper.layout2px(layout);
           const contentHeight = flItem.getHeight(element, pixels.width);
-          if (contentHeight > pixels.height) {
+          if (flItem.resizable <= 1 || contentHeight > pixels.height) {
             pixels.height = contentHeight;
             return flContainer.mapper.px2layout(pixels);
           }
