fb25e349ec3de2e4ed73e8933e49140ecfa2658a
Nalin Ilango <nalin@theteamie.com>
2017-02-03 16:37:40 +0800

    If size is restricted allow reducing item height

--- src/app/directives/fl-item.js
+++ src/app/directives/fl-item.js
@@ -154,7 +154,7 @@ export default function () {
         if (flItem.getHeight) {
           const pixels = flContainer.mapper.layout2px(layout);
           const contentHeight = flItem.getHeight(element, pixels.width, isFreeSize);
-          if (resizeOption < 2 || contentHeight > pixels.height) {
+          if (resizeOption < 2 || !isFreeSize || contentHeight > pixels.height) {
             pixels.height = contentHeight;
             return flContainer.mapper.px2layout(pixels);
           }
