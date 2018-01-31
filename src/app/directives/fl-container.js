d4f833d5e8feb40a760046c9ef14f27eeece76a4
SudarsanGP <sudarsangp@gmail.com>
2018-01-31 14:02:27 +0800

    use ternary operator for spacing

--- src/app/directives/fl-container.js
+++ src/app/directives/fl-container.js
@@ -57,11 +57,7 @@ export default function () {
 
       render() {
         this.container.removeGaps();
-        if (this.isEditable) {
-          this.$element.css('height', this.mapper.height2px(this.container.getMaxHeight()) + 100);
-        } else {
-          this.$element.css('height', this.mapper.height2px(this.container.getMaxHeight()));
-        }
+        this.$element.css('height', this.mapper.height2px(this.container.getMaxHeight()) + (this.isEditable ? 100 : 0));
         this.flItems.forEach(flItem => flItem.render(this.mapper.layout2px(flItem.item), {
           left: flItem.item.left,
           top: flItem.item.top,
